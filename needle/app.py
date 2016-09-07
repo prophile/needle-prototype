0c5fd5e8b180c51eafde20fbe3da796ac1ecf5e2
Alistair Lynn <arplynn@gmail.com>
2016-09-07 20:12:07 +0100

    Add a send_static_file utility

--- needle/app.py
+++ needle/app.py
@@ -4,11 +4,24 @@ import pkg_resources
 app = flask.Flask('needle')
 
 
+def send_static_file(path, mimetype='text/html'):
+    file_data = pkg_resources.resource_string('needle', 'static/index.html')
+
+    response = flask.Response(
+        status=200,
+        mimetype=mimetype,
+    )
+
+    response.set_data(file_data)
+
+    response.add_etag()
+    response.make_conditional(flask.request)
+
+    response.headers['Cache-Control'] = 'max-age: 30'
+
+    return response
+
+
 @app.route('/')
 def bees():
-    return flask.send_file(
-        pkg_resources.resource_stream('needle', 'static/index.html'),
-        mimetype='text/html',
-        conditional=True,
-        cache_timeout=30,
-    )
+    return send_static_file('static/index.html')
