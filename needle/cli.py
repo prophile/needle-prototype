0f0b038f5f4b01e15d6b643d52c2269819046086
Alistair Lynn <arplynn@gmail.com>
2016-09-07 19:05:27 +0100

    Add stub argument parser

--- needle/cli.py
+++ needle/cli.py
@@ -1,4 +1,11 @@
 import sys
+import argparse
 
-def main():
-    print(sys.argv)
+def argument_parser():
+    parser = argparse.ArgumentParser(description="An A/B test server")
+
+    return parser
+
+def main(args=sys.argv[1:]):
+    options = argument_parser().parse_args(args)
+    print(options)
