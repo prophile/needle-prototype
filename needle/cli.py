9d027053d59c54aa4e67cfcb22acf9ab7eaeed95
Alistair Lynn <arplynn@gmail.com>
2016-09-07 19:12:26 +0100

    Add expected arguments

--- needle/cli.py
+++ needle/cli.py
@@ -1,8 +1,31 @@
 import sys
+import pathlib
 import argparse
 
 def argument_parser():
     parser = argparse.ArgumentParser(description="An A/B test server")
+
+    parser.add_argument(
+        'dir',
+        type=pathlib.Path,
+        default=pathlib.Path.cwd(),
+        help="main directory, defining tests and metrics",
+    )
+
+    parser.add_argument(
+        "-p",
+        "--port",
+        type=int,
+        default=1212,
+        help="port on which to run the HTTP server",
+    )
+
+    parser.add_argument(
+        '-v',
+        '--verbose',
+        action='store_true',
+        help="be particularly noisy",
+    )
 
     return parser
 
