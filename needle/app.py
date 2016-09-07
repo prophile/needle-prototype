f102eddd097cd9a9f81a613ee28c7f744ca3545c
Alistair Lynn <arplynn@gmail.com>
2016-09-07 20:01:56 +0100

    Move this into static/

--- needle/app.py
+++ needle/app.py
@@ -7,7 +7,7 @@ app = flask.Flask('needle')
 @app.route('/')
 def bees():
     return flask.send_file(
-        pkg_resources.resource_stream('needle', 'index.html'),
+        pkg_resources.resource_stream('needle', 'static/index.html'),
         mimetype='text/html',
         conditional=True,
         cache_timeout=30,
