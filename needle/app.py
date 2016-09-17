796202f50c9a4fe0686e40b8ab0068c16bc28411
Alistair Lynn <arplynn@gmail.com>
2016-09-17 12:41:44 +0100

    Run reports in the background

--- needle/app.py
+++ needle/app.py
@@ -1,14 +1,19 @@
 import json
 import aiohttp.web
+import asyncio
 import logging
 import dateutil.parser
 import functools
+import concurrent.futures
 import pkg_resources
 
 from .experiment import user_experiments
 
 
 logger = logging.getLogger(__name__)
+
+
+current_results = {}
 
 
 def get_configuration(request):
@@ -89,11 +94,36 @@ def get_app(root, *, debug=False):
     return app
 
 
+def bg_run_reports(path):
+    from .report import run_all_reports  # Lazy load
+    from .configuration import Configuration  # Lazy-load
+    import logging
+    logging.basicConfig(level=logging.DEBUG)
+    config = Configuration(path)
+    return run_all_reports(config)
+
+
+def bg_reports_task(loop, path):
+    executor = concurrent.futures.ProcessPoolExecutor()
+    future = loop.run_in_executor(executor, bg_run_reports, path)
+
+    def done_a_thing(x):
+        current_results.update(x.result())
+        loop.call_later(30, bg_reports_task, loop, path)
+    future.add_done_callback(done_a_thing)
+
+
 def run(root, *, host='::', port=2121, debug=False):
     app = get_app(root, debug=debug)
 
-    aiohttp.web.run_app(
-        app,
-        host=host,
-        port=port,
+    loop = asyncio.get_event_loop()
+
+    server = loop.create_server(
+        app.make_handler(),
+        host,
+        port,
     )
+
+    loop.call_soon(bg_reports_task, loop, root)
+
+    loop.run_forever()
